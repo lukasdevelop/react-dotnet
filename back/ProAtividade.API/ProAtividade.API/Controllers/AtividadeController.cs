db4a718d9770d6176029dd0bba6322cd49a1fac5
Lucas Amaral <analista.sistemas.lucas@gmail.com>
2023-03-29 18:08:14 -0300

    Primerio commit

---
+++ back/ProAtividade.API/ProAtividade.API/Controllers/AtividadeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProAtividade.API.Models;
+
+namespace ProAtividade.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AtividadeController : ControllerBase
+    {
+        [HttpGet]
+        public Atividade get()
+        {
+            return new Atividade();
+        }
+        [HttpGet("{id}")]
+        public string get(int id)
+        {
+            return $"Minha controler {id}";
+        }
+        [HttpPost]
+        public Atividade post(Atividade atividade)
+        {
+            return atividade;
+        }
+        [HttpPut]
+        public string put()
+        {
+            return "Minha controler";
+        }
+        [HttpDelete]
+        public string delete()
+        {
+            return "Minha controler";
+        }
+    }
+}
---
+++ back/ProAtividade.API/ProAtividade.API/Models/Atividade.cs
@@ -0,0 +1,11 @@
+﻿namespace ProAtividade.API.Models
+{
+    public class Atividade
+    {
+        public int Id { get; set; }
+
+        public string Titulo { get; set; }
+        public string Descricao { get; set; }
+        public string Prioridade { get; set; }
+    }
+}
